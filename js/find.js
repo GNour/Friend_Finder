6901c6f0ec2167c146a0fb5eae22827620d16dde
Ghyath Noureddine <ghyathnour@gmail.com>
2021-09-20 23:27:59 +0300

    Add find feature with add friend and block user

---
+++ js/find.js
@@ -0,0 +1,116 @@
+let count = 0;
+let resultsCount = 0;
+
+$("#searchBox").keyup((event) => {
+  if ($("#searchBox").val().length % 3 == 0 && $("#searchBox").val() != "") {
+    searchQuery($("#searchBox").val()).then((result) => {
+      console.log(result);
+      appendSearchResult(result);
+    });
+  } else if ($("#searchBox").val() == "") {
+    count = 0;
+  }
+});
+
+async function searchQuery(query) {
+  const response = await fetch(
+    "php/API/find.php?action=querySearch&search=" + query
+  );
+
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+
+  const results = await response.json();
+
+  return results;
+}
+
+async function findAPI(action, id, date) {
+  const response = await fetch(
+    "php/API/find.php?action=" + action + "&id=" + id + "&date=" + date
+  );
+
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+
+  const results = await response.json();
+
+  return results;
+}
+
+function appendSearchResult(result) {
+  const searchContainer = document.getElementById("searchResult");
+  searchContainer.innerHTML = "";
+  for (const [key, value] of Object.entries(result)) {
+    const userCard = `
+        <div class="nearby-user" id="user_${key}">
+                <div class="row">
+                  <div class="col-md-2 col-sm-2">
+                    <img
+                      src="images/users/user-15.jpg"
+                      alt="user"
+                      class="profile-photo-lg"
+                    />
+                  </div>
+                  <div class="col-md-7 col-sm-7">
+                    <h5><a href="#" class="profile-link">${value.first_name} ${value.last_name}</a></h5>
+                    <p class="text-muted">${value.city}, ${value.country}</p>
+                  </div>
+                  <div class="col-md-3 col-sm-3">
+                    <button class="btn btn-primary mb-5 addButton" id="${key}">Add Friend</button>
+                    <button class="btn btn-warning mb-5 blockButton" id="${key}">Block</button>
+                  </div>
+                </div>
+              </div>`;
+
+    searchContainer.innerHTML += userCard;
+    resultsCount++;
+  }
+  updateHeader(resultsCount);
+  $(".addButton").click(sendRequest);
+  $(".blockButton").click(blockUser);
+}
+
+function sendRequest(event) {
+  const id = event.currentTarget.id;
+  const action = "createRequest";
+
+  findAPI(action, id).then((result) => {
+    if (result.ok == 200) {
+      removeUserCard(id);
+      resultsCount--;
+      updateHeader(resultsCount);
+    }
+  });
+}
+
+function blockUser(event) {
+  const id = event.currentTarget.id;
+  const action = "block";
+
+  findAPI(action, id).then((result) => {
+    if (result.ok == 200) {
+      removeUserCard(id);
+      resultsCount--;
+      updateHeader(resultsCount);
+    }
+  });
+}
+
+function removeUserCard(id) {
+  $("#user_" + id).fadeOut(100, function () {
+    $(this).remove();
+  });
+}
+
+function updateHeader(count) {
+  if (count > 0) {
+    $("#searchHeader").html("Found " + count + " Results");
+  } else {
+    $("#searchHeader").html("Search for friends");
+  }
+}
---
+++ php/API/find.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once "../base/User.php";
+require_once "../base/Notification.php";
+require_once "../config/connection.php";
+
+session_start();
+$userId = $_SESSION["user"]->getId();
+if ($_GET["action"] == "querySearch") {
+
+    $query = $_GET["search"];
+    $searchQuery = $connection->query("SELECT *\n"
+
+        . "FROM user as u\n"
+
+        . "WHERE \n"
+
+        . "(u.first_name LIKE \"" . $query . "%\" OR u.last_name LIKE \"" . $query . "%\")\n"
+
+        . "AND \n"
+
+        . "u.id NOT IN(SELECT friend_id FROM user_friend_list WHERE user_id = " . $userId . ")\n"
+
+        . "AND u.id NOT IN(SELECT friend_id FROM user_block_list WHERE user_id = " . $userId . ")\n"
+
+        . "AND u.id NOT IN(SELECT to_user FROM notification WHERE from_user = " . $userId . " AND response = -1)\n"
+
+        . "AND u.id != " . $userId);
+
+    $result = array();
+
+    while ($row = $searchQuery->fetch_assoc()) {
+        $temp["id"] = $row["id"];
+        $temp["first_name"] = $row["first_name"];
+        $temp["last_name"] = $row["last_name"];
+        $temp["city"] = $row["city"];
+        $temp["country"] = $row["country"];
+
+        $result[$row["id"]] = $temp;
+    }
+
+    echo json_encode($result);
+}
+
+if ($_GET["action"] == "block") {
+    if ($_SESSION["user"]->blockUser($_GET["id"])) {
+        echo json_encode(array("ok" => 200, "message" => "Blocked User"));
+    }
+}
+
+if ($_GET["action"] == "createRequest") {
+    echo json_encode(Notification::createRequest($userId, $_GET["id"], $_GET["date"]));
+}
